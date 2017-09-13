a7f2ab7b52ce7a544db7649fae3dc957235bf825
sunticity <31881524+sunticity@users.noreply.github.com>
2017-09-13 09:32:24 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,76 +1,18 @@
 <?php
-/*
+// Created by Sudjai Prompol
+// Created date:15 AUG 2017
 $strAccessToken = "GikeiA/YVAwtlfEn+6owfwFp+WGlnG4OobmCQO3G49uokliuCEZtM8DCNSmrRoXDUCnkX8k6zrqEHd8teFp6Pc55kUZCE7XBuiGFuUOBtq4/JSrv1bLl2Wk0b+3xt3R9yWzVVXlyrtPogPA/fM68awdB04t89/1O/w1cDnyilFU=";
- 
+
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
+
 $strUrl = "https://api.line.me/v2/bot/message/reply";
 
-//$strUrl = "https://api.line.me/v1/oauth/verify";
- 
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
- 
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง" or 
-         $arrJson['events'][0]['message']['text'] == "ทำอะไร" or 
-         $arrJson['events'][0]['message']['text'] == "ทำไร"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+$_msg = $arrJson['events'][0]['message']['text'];
 
-}else if($arrJson['events'][0]['message']['text'] == "อากาศ")
-{//ถ้าพิมพ์มาว่า อากาศ ก็ให้ไปดึง API จาก wunderground มา
-            //http://api.wunderground.com/api/yourkey/forecast/lang:TH/q/Thailand/%E0%B8%81%E0%B8%A3%E0%B8%B8%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%9E%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A3.json
-            $ch1 = curl_init();
-            curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch1, CURLOPT_URL, 'http://api.wunderground.com/api/yourkey/forecast/lang:TH/q/Thailand/'.str_replace(' ', '%20', $text_ex[1]).'.json');
-            $result1 = curl_exec($ch1);
-            curl_close($ch1);
-            
-            $obj = json_decode($result1, true);
-            if(isset($obj['forecast']['txt_forecast']['forecastday'][0]['fcttext_metric'])){
-                $result_text = $obj['forecast']['txt_forecast']['forecastday'][0]['fcttext_metric'];
-            }else{//ถ้าไม่เจอกับตอบกลับว่าไม่พบข้อมูล
-                $result_text = 'ไม่พบข้อมูล';
-            }
-            
-            $response_format_text = ['contentType'=>1,"toType"=>1,"text"=>$result_text];
-//---
 
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = $response_format_text; 
-}else{
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
-}
- 
- 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close ($ch);
-*/ 
+
 ?>
